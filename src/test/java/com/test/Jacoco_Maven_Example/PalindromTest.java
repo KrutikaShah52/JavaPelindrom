df12bde8985f78c0b9fea64c25faa43b77633d7b
Krutika Shah <krutika.shah@einfochips.com>
2018-04-16 12:17:44 +0530

    update changes

--- src/test/java/com/test/Jacoco_Maven_Example/PalindromTest.java
+++ src/test/java/com/test/Jacoco_Maven_Example/PalindromTest.java
@@ -2,6 +2,7 @@ package com.test.Jacoco_Maven_Example;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
@@ -17,4 +18,16 @@ public class PalindromTest {
 	    PalindromeExample palindromeTester = new PalindromeExample();
 	    assertFalse(palindromeTester.isPalindrome("sir"));
 	}
+	
+//	@Test
+//	public void whenPalinrome_thenAccept(){
+//	    PalindromeExample palindromeTester = new PalindromeExample();
+//	    assertEquals(true, palindromeTester.isPalindrome("aba"));
+//	}
+	
+//	@Test
+//	public void NearPalindrom_thanReject(){
+//	    PalindromeExample palindromeTester = new PalindromeExample();
+//	    assertFalse(palindromeTester.isPalindrome("abcdef"));
+//	}
 }
